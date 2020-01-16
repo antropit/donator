bd6657fb4f47800cd5198343654c54da43213f7a
antropit <antropit@hotmail.com>
2020-01-16 23:49:49 +0300

    all new donate form

--- src/components/Donator.vue
+++ src/components/Donator.vue
@@ -1,84 +1,74 @@
 <template>
-  <v-container>
+  <v-container
+    fluid
+    fill-height
+  >
     <v-layout
-      text-center
-      wrap
+      align-center
+      justify-center
     >
-      <v-flex xs12>
-        <v-img
-          :src="require('../assets/logo.svg')"
-          class="my-3"
-          contain
-          height="200"
-        ></v-img>
-      </v-flex>
-
-      <v-flex mb-4>
-        <h1 class="display-2 font-weight-bold mb-3">
-          Welcome to Vuetify
-        </h1>
-        <p class="subheading font-weight-regular">
-          For help and collaboration with other Vuetify developers,
-          <br>please join our online
-          <a href="https://community.vuetifyjs.com" target="_blank">Discord Community</a>
-        </p>
-      </v-flex>
-
-      <v-flex
-        mb-5
-        xs12
-      >
-        <h2 class="headline font-weight-bold mb-3">What's next?</h2>
-
-        <v-layout justify-center>
-          <a
-            v-for="(next, i) in whatsNext"
-            :key="i"
-            :href="next.href"
-            class="subheading mx-3"
-            target="_blank"
-          >
-            {{ next.text }}
-          </a>
-        </v-layout>
-      </v-flex>
-
       <v-flex
         xs12
-        mb-5
+        sm8
+        md4
       >
-        <h2 class="headline font-weight-bold mb-3">Important Links</h2>
-
-        <v-layout justify-center>
-          <a
-            v-for="(link, i) in importantLinks"
-            :key="i"
-            :href="link.href"
-            class="subheading mx-3"
-            target="_blank"
+        <v-card class="elevation-12">
+          <v-toolbar
+            color="primary"
+            dark
+            flat
           >
-            {{ link.text }}
-          </a>
-        </v-layout>
-      </v-flex>
-
-      <v-flex
-        xs12
-        mb-5
-      >
-        <h2 class="headline font-weight-bold mb-3">Ecosystem</h2>
-
-        <v-layout justify-center>
-          <a
-            v-for="(eco, i) in ecosystem"
-            :key="i"
-            :href="eco.href"
-            class="subheading mx-3"
-            target="_blank"
-          >
-            {{ eco.text }}
-          </a>
-        </v-layout>
+            <v-toolbar-title>Donate form</v-toolbar-title>
+            <v-spacer></v-spacer>
+          </v-toolbar>
+          <v-container grid-list-md align-center>
+            <v-layout row wrap>
+                <v-flex v-for="(next, i) in $store.state.presets" :key="i" xs4>
+                  <v-btn
+                    :color= "i == $store.state.presetActiveId ? 'primary' : 'normal'"
+                    id="`btn${i}`"
+                    width="100%"
+                    @click="presetClick(i)"
+                  >{{ next.toLocaleString('en-US', { style: 'currency', currency: $store.state.currency, minimumFractionDigits: 0, maximumFractionDigits: 0 }) }}</v-btn>
+                </v-flex>
+            </v-layout>
+          </v-container>
+          <v-card-text>
+            <v-form
+              v-on:submit.prevent="onSubmit"
+            >
+              <v-text-field
+                v-model="$store.state.sum"
+                id="v-text-field"
+                label="sum"
+                name="sum"
+                type="number"
+                :prefix="$store.state.currencySymbol"
+                :suffix="$store.state.currency"
+                :append-icon="$store.state.showCurrencies ? 'fa-angle-up' : 'fa-angle-down'"
+                @change="inputChange"
+                @click:append="clickAppend"
+              ></v-text-field>
+            </v-form>
+            <v-list v-if="$store.state.showCurrencies">
+              <v-list-item-group v-model="$store.state.currencyActiveId" color="primary">
+                <v-list-item
+                  v-for="(item, i) in $store.getters.getCurrencies"
+                  :key="i"
+                  :inactive="inactive"
+                  @click="currencySelected(i)"
+                >
+                  <v-list-item-content>
+                    <v-list-item-title v-html="item.name"></v-list-item-title>
+                  </v-list-item-content>
+                </v-list-item>
+              </v-list-item-group>
+            </v-list>
+          </v-card-text>
+          <v-card-actions>
+            <v-btn color="primary" id="donate" width="100%">DONATE</v-btn>
+          </v-card-actions>
+        </v-card>
       </v-flex>
     </v-layout>
   </v-container>
@@ -89,56 +79,22 @@ export default {
   name: 'Donator',
 
   data: () => ({
-    ecosystem: [
-      {
-        text: 'vuetify-loader',
-        href: 'https://github.com/vuetifyjs/vuetify-loader',
-      },
-      {
-        text: 'github',
-        href: 'https://github.com/vuetifyjs/vuetify',
-      },
-      {
-        text: 'awesome-vuetify',
-        href: 'https://github.com/vuetifyjs/awesome-vuetify',
-      },
-    ],
-    importantLinks: [
-      {
-        text: 'Documentation',
-        href: 'https://vuetifyjs.com',
-      },
-      {
-        text: 'Chat',
-        href: 'https://community.vuetifyjs.com',
-      },
-      {
-        text: 'Made with Vuetify',
-        href: 'https://madewithvuejs.com/vuetify',
-      },
-      {
-        text: 'Twitter',
-        href: 'https://twitter.com/vuetifyjs',
-      },
-      {
-        text: 'Articles',
-        href: 'https://medium.com/vuetify',
-      },
-    ],
-    whatsNext: [
-      {
-        text: 'Explore components',
-        href: 'https://vuetifyjs.com/components/api-explorer',
-      },
-      {
-        text: 'Select a layout',
-        href: 'https://vuetifyjs.com/layout/pre-defined',
-      },
-      {
-        text: 'Frequently Asked Questions',
-        href: 'https://vuetifyjs.com/getting-started/frequently-asked-questions',
-      },
-    ],
+    //
   }),
+  methods: {
+    presetClick(index) {
+      this.$store.commit('onPresetsClick', index)
+    },
+    inputChange(value) {
+      value = Math.floor(value);
+      this.$store.commit('onInputChange', value);
+    },
+    clickAppend(event) {
+      this.$store.commit('onClickAppend');
+    },
+    currencySelected(value) {
+      this.$store.commit('onCurrencySelected', value);
+    },
+  }
 };
 </script>
--- src/store/index.js
+++ src/store/index.js
@@ -1,12 +1,73 @@
 import Vue from 'vue';
 import Vuex from 'vuex';
 
+const presets = [40, 100, 200, 1000, 2500, 5000];
+const suggestion = 40;
+
+const currencies = [
+	{name: "US Dollar", code: "USD", symbol: "$", rate: 1},
+	{name: "Euro", code: "EUR", symbol: "€", rate: 0.897597},
+	{name: "British Pound", code: "GBP", symbol: "£", rate: 0.81755},
+	{name: "Russian Ruble", code: "RUB", symbol: "₽", rate: 63.461993},
+];
+
+
 Vue.use(Vuex);
 
 export default new Vuex.Store({
   state: {
+    sum: suggestion,
+    presetActiveId: 0,
+    currency: "USD",
+    currencySymbol: "$",
+    currencyRate: 1,
+    currencyActiveId: 0,
+    showCurrencies: false,
+    presets
+  },
+  getters: {
+    getCurrencies: () => currencies,
+    getCurrencyData: state => currencies.find(it => it.code === state.currency),
   },
   mutations: {
+    onPresetsClick(state, id) {
+      state.presetActiveId = id;
+      state.sum = state.presets[id];
+    },
+    onInputChange(state, value) {
+      state.sum = value;
+      state.presetActiveId = state.presets.findIndex(it => it == value);
+    },
+    onClickAppend(state) {
+      state.showCurrencies = !state.showCurrencies;
+    },
+    onCurrencySelected(state, id) {
+      state.showCurrencies = false;
+      state.currencyActiveId = id;
+      state.currency = currencies[id].code;
+      state.currencySymbol = currencies[id].symbol;
+
+      if(id > 0) {
+        state.presets = presets.map(val => {
+          val = Math.floor(val * currencies[id].rate);
+          if(val <= 150) val = Math.round(val / 30) * 30;
+          else if(val <= 600) val = Math.round(val / 50) * 50;
+          else if(val <= 5000) val = Math.round(val / 500) * 500;
+          else if(val <= 50000) val = Math.round(val / 5000) * 5000;
+          else val = Math.round(val / 50000) * 50000;
+
+          return val;
+        });
+      } else state.presets = presets;
+
+      if(state.presetActiveId != undefined) {
+        state.sum = state.presets[state.presetActiveId];
+      } else { 
+        state.sum = Math.floor(state.sum * currencies[id].rate / state.currencyRate);
+      }
+
+      state.currencyRate = currencies[id].rate;
+    },
   },
   actions: {
   },
